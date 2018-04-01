20d7a5b6801a61df5e111d84301b8569a2a62a75
joriscoppieters <joris.coppieters@gmail.com>
2018-04-01 22:42:45 +1200

    Set version to 1.7.5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // ******************************
 //
 //
-// TORIS FORMAT v1.7.4
+// TORIS FORMAT v1.7.5
 //
 // Version History:
 //
@@ -82,7 +82,7 @@ var test = require('./src/test');
 // Constants:
 // ******************************
 
-const k_VERSION = '1.7.4';
+const k_VERSION = '1.7.5';
 
 // ******************************
 // TODO: DEPRECATE
